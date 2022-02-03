d8539f08a3f06593bbd5631e021f0d701f480c7b
Alexey Bilko <bilkoalexey67@gmail.com>
2022-02-03 15:30:13 +0200

    created empty projects for task1 and task2

---
+++ task2.lang_with_go_to/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace task2.lang_with_go_to
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
